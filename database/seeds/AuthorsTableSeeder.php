cde0c44bda3156351097136d423ad85dfedae3de
Viola Ceni <viola.ceni@cit.edu.al>
2019-12-27 14:23:46 +0100

    Created Author seeder

---
+++ database/seeds/AuthorsTableSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+use App\Author;
+use Illuminate\Database\Seeder;
+
+class AuthorsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        
+        factory(Author::class, 10)->create();
+    }
+}
