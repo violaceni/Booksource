4b94cd135df661dd5f64103e5ad099e1285364dd
Viola Ceni <viola.ceni@cit.edu.al>
2020-06-25 13:03:39 +0200

    feat(mix): add mix and custom css style

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,39 @@
 require('./bootstrap');
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Homepage from './components/Homepage'
+import Datasource from './components/Datasource'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+// Install BootstrapVue
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
+
+
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+    mode: 'history',
+    routes: [
+        {
+            path: '/booksource',
+            name: 'Homepage',
+            component: Homepage,
+            props: true
+        },
+        {
+            path: '/booksource/index',
+            name: 'datasource',
+            component: Datasource,
+            props: true
+        },
+    ],
+});
+
+const app = new Vue({
+    el: '#app',
+    router,
+    components: { Homepage },
+});
